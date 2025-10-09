c4c43f2a3f5c8ed536c72a124794074cbcc14743
pawan kshetri <pawankshetri11@gmail.com>
2025-10-09 20:09:51 +0530

    delte

--- Sql/ADD_REJECTION_REASON_COLUMN.sql
+++
@@ -1,8 +0,0 @@
--- Add kyc_rejection_reason column to profiles table
--- This column stores the reason when admin rejects a KYC submission
-
-ALTER TABLE public.profiles 
-ADD COLUMN IF NOT EXISTS kyc_rejection_reason TEXT;
-
--- Add comment for documentation
-COMMENT ON COLUMN public.profiles.kyc_rejection_reason IS 'Reason provided by admin when rejecting KYC submission';
